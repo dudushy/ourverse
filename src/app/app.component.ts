882f2b35155566b1826fcd0827c3f78801608fdd
dudushy <eduardotalarico1337@gmail.com>
2023-03-23 21:43:14 -0300

    Function addStar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,10 @@ export class AppComponent {
     this.window.onresize = () => {
       this.toggleMobileMode();
     };
+
+    if (this.db.get('stars') === null) {
+      this.db.set('stars', []);
+    }
   }
 
   updateView(from: string) {
@@ -75,5 +79,35 @@ export class AppComponent {
     }
 
     this.updateView(this.title);
+  }
+
+  addStart(event: any) {
+    console.log(`[${this.title}#addStart] event`, event);
+
+    const Xpos = event.offsetX;
+    console.log(`[${this.title}#addStart] Xpos`, Xpos);
+
+    const Ypos = event.offsetY;
+    console.log(`[${this.title}#addStart] Ypos`, Ypos);
+
+    const newStar = document.createElement('button');
+    newStar.id = 'star';
+    newStar.style.top = Ypos + 'px';
+    newStar.style.left = Xpos + 'px';
+    newStar.style.position = 'absolute';
+    newStar.style.width = '3px';
+    newStar.style.height = '3px';
+    newStar.style.borderRadius = '50%';
+    newStar.style.border = 'none';
+    newStar.style.padding = '0';
+    newStar.style.margin = '0';
+    newStar.style.backgroundColor = 'white';
+    newStar.style.boxShadow = '0px 0px 10px 2px white';
+    newStar.style.zIndex = '1';
+
+    this.db.get('stars')?.push(newStar);
+
+    const main = document.querySelector('main');
+    main?.appendChild(newStar);
   }
 }
